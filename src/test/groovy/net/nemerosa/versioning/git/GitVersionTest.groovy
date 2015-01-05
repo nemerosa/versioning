64e456a1b5315bfcb9f0a54c5b73f9500dd874ac
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-05 17:18:15 +0100

    Git test on release branch with no previous tag

--- src/test/groovy/net/nemerosa/versioning/git/GitVersionTest.groovy
+++ src/test/groovy/net/nemerosa/versioning/git/GitVersionTest.groovy
@@ -59,4 +59,38 @@ class GitVersionTest {
         }
     }
 
+    @Test
+    void 'Git release branch: no previous tag'() {
+        GitRepo repo = new GitRepo()
+        try {
+            // Git initialisation
+            repo.with {
+                git 'init'
+                (1..4).each { commit it }
+                git 'checkout', '-b', 'release/2.0'
+                commit 5
+                git 'log', '--oneline', '--graph', '--decorate', '--all'
+            }
+            def head = repo.commitLookup('Commit 5')
+            def headAbbreviated = repo.commitLookup('Commit 5', true)
+
+            def project = ProjectBuilder.builder().withProjectDir(repo.dir).build()
+            new VersioningPlugin().apply(project)
+            VersionInfo info = project.versioning.info as VersionInfo
+            assert info != null
+            assert info.build == headAbbreviated
+            assert info.branch == 'release/2.0'
+            assert info.base == '2.0'
+            assert info.branchId == 'release-2.0'
+            assert info.branchType == 'release'
+            assert info.commit == head
+            assert info.display == '2.0.0'
+            assert info.full == "release-2.0-${headAbbreviated}"
+            assert info.scm == 'git'
+
+        } finally {
+            repo.close()
+        }
+    }
+
 }
