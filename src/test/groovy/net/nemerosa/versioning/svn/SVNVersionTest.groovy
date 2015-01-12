64e2e86a1ce4582fa07161b97b7e78c58a52c5f2
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2015-01-12 13:24:57 +0100

    #6 SVN tests for release branches

--- src/test/groovy/net/nemerosa/versioning/svn/SVNVersionTest.groovy
+++ src/test/groovy/net/nemerosa/versioning/svn/SVNVersionTest.groovy
@@ -377,4 +377,67 @@ VERSION_SCM = svn
         assert info.scm == 'svn'
     }
 
+    @Test
+    void 'SVN: release branch: no previous tag'() {
+
+        // SVN
+        repo.mkdir 'project/branches/release-2.0', 'Feature branch'
+        repo.mkdir 'project/branches/release-2.0/1', 'Commit for TEST-1'
+        repo.mkdir 'project/branches/release-2.0/2', 'Commit for TEST-1'
+
+        // Project
+        def project = ProjectBuilder.builder().withProjectDir(SVNRepo.checkout('project/branches/release-2.0')).build()
+        new VersioningPlugin().apply(project)
+        project.versioning {
+            scm = 'svn'
+        }
+
+        // Gets the info and checks it
+        VersionInfo info = project.versioning.info as VersionInfo
+        assert info != null
+        assert info.build == '3'
+        assert info.branch == 'release-2.0'
+        assert info.base == '2.0'
+        assert info.branchId == 'release-2.0'
+        assert info.branchType == 'release'
+        assert info.commit == '3'
+        assert info.display == "2.0.0"
+        assert info.full == "release-2.0-3"
+        assert info.scm == 'svn'
+
+    }
+
+    @Test
+    void 'SVN: release branch: with previous tag'() {
+
+        // SVN
+        repo.mkdir 'project/branches/release-2.0', 'Feature branch'
+        repo.mkdir 'project/branches/release-2.0/1', 'Commit for TEST-1' // Tag
+        repo.mkdir 'project/branches/release-2.0/2', 'Commit for TEST-1'
+
+        // Tagging
+        repo.copy 'project/branches/release-2.0@2', 'project/tags/2.0.1', 'v2.0.1'
+
+        // Project
+        def project = ProjectBuilder.builder().withProjectDir(SVNRepo.checkout('project/branches/release-2.0')).build()
+        new VersioningPlugin().apply(project)
+        project.versioning {
+            scm = 'svn'
+        }
+
+        // Gets the info and checks it
+        VersionInfo info = project.versioning.info as VersionInfo
+        assert info != null
+        assert info.build == '3'
+        assert info.branch == 'release-2.0'
+        assert info.base == '2.0'
+        assert info.branchId == 'release-2.0'
+        assert info.branchType == 'release'
+        assert info.commit == '3'
+        assert info.display == "2.0.1"
+        assert info.full == "release-2.0-3"
+        assert info.scm == 'svn'
+
+    }
+
 }
