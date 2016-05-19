6ca20ed6c060d8a61ae22907fc042b6fcba0be11
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-05-19 21:36:53 +0200

    SVN - collection of tags

--- src/main/groovy/net/nemerosa/versioning/svn/SVNInfoService.groovy
+++ src/main/groovy/net/nemerosa/versioning/svn/SVNInfoService.groovy
@@ -3,12 +3,11 @@ package net.nemerosa.versioning.svn
 import net.nemerosa.versioning.SCMInfo
 import net.nemerosa.versioning.SCMInfoService
 import net.nemerosa.versioning.VersioningExtension
-import net.nemerosa.versioning.support.ProcessExitException
 import org.gradle.api.Project
 import org.tmatesoft.svn.core.SVNDepth
+import org.tmatesoft.svn.core.SVNDirEntry
+import org.tmatesoft.svn.core.SVNURL
 import org.tmatesoft.svn.core.wc.*
-
-import static net.nemerosa.versioning.support.Utils.run
 
 class SVNInfoService implements SCMInfoService {
 
@@ -87,12 +86,15 @@ class SVNInfoService implements SCMInfoService {
 
     @Override
     List<String> getBaseTags(Project project, VersioningExtension extension, String base) {
-        // Gets the SVN raw info as XML
-        String xmlInfo = run(project.projectDir, 'svn', 'info', '--xml')
-        // Parsing
-        def info = new XmlSlurper().parseText(xmlInfo)
+        // Gets the client manager
+        def clientManager = getClientManager(extension)
+        // Gets the SVN information
+        SVNInfo info = clientManager.getWCClient().doInfo(
+                project.projectDir,
+                SVNRevision.HEAD
+        )
         // URL
-        String url = info.entry.url as String
+        String url = info.URL as String
         // Branch parsing
         String branch = parseBranch(url)
         // Gets the base URL by removing the branch
@@ -103,51 +105,34 @@ class SVNInfoService implements SCMInfoService {
             baseUrl = url - "branches/${branch}"
         }
         // Gets the list of tags
-        try {
-            String tagsUrl = "${baseUrl}/tags"
-            println "[version] Getting list of tags from ${tagsUrl}..."
-            // Command arguments
-            List<String> args = ['list', '--xml', '--non-interactive']
-            // Credentials
-            if (extension.user) {
-                println "[version] Authenticating with ${extension.user}"
-                args.addAll([
-                        '--no-auth-cache',
-                        '--username', extension.user,
-                        '--password', extension.password,
-                ])
-            }
-            // Certificate
-            if (extension.trustServerCert) {
-                println "[version] Trusting certificate by default"
-                args << '--trust-server-cert'
-            }
-            // Tags folder
-            args << tagsUrl
-            // Command
-            def xmlTags = run(project.projectDir, 'svn', args as String[])
-            // Parsing
-            def lists = new XmlSlurper().parseText(xmlTags)
-            // Lists of tags, order from the most recent to the oldest
-            List<String> tags = lists.list.entry.sort { -(it.commit.@revision.text() as long) }.collect { it.name }
-            // Keeping only tags which fit the release pattern
-            def baseTagPattern = /(${base}\.(\d+))/
-            return tags.collect { tag ->
-                def m = tag =~ baseTagPattern
-                if (m.find()) {
-                    m.group(1)
-                } else {
-                    ''
-                }
-            }.findAll { it != '' }
-        } catch (ProcessExitException ex) {
-            if (ex.exit == 1 && ex.message.contains('E200009')) {
-                println "[version] The tags/ folder does not exist yet"
-                return []
-            } else {
-                throw ex
-            }
+        String tagsUrl = "${baseUrl}/tags"
+        println "[version] Getting list of tags from ${tagsUrl}..."
+        // Gets the list
+        List<SVNDirEntry> entries = []
+        clientManager.logClient.doList(
+                SVNURL.parseURIEncoded(tagsUrl),
+                SVNRevision.HEAD,
+                SVNRevision.HEAD,
+                false,
+                false,
+                { dirEntry -> entries.add(dirEntry) }
+        )
+        // Lists of tags, order from the most recent to the oldest
+        List<String> tags = entries.sort {
+            -it.revision
+        }.collect {
+            it.name
         }
+        // Keeping only tags which fit the release pattern
+        def baseTagPattern = /(${base}\.(\d+))/
+        return tags.collect { tag ->
+            def m = tag =~ baseTagPattern
+            if (m.find()) {
+                m.group(1)
+            } else {
+                ''
+            }
+        }.findAll { it != '' }
     }
 
     @Override
@@ -161,6 +146,20 @@ class SVNInfoService implements SCMInfoService {
     protected static SVNClientManager getClientManager(VersioningExtension extension) {
         return SVNClientManager.newInstance()
         // TODO Authentication
+        // Credentials
+//        if (extension.user) {
+//            println "[version] Authenticating with ${extension.user}"
+//            args.addAll([
+//                    '--no-auth-cache',
+//                    '--username', extension.user,
+//                    '--password', extension.password,
+//            ])
+//        }
+//        // Certificate
+//        if (extension.trustServerCert) {
+//            println "[version] Trusting certificate by default"
+//            args << '--trust-server-cert'
+//        }
     }
 
 }
