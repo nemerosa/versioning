9dd250f871bd13b23c438499cd3ae1f4674ffd38
Damien Coraboeuf <damien.coraboeuf@gmail.com>
2016-05-21 18:03:30 +0200

    Removed command line utilities

--- src/main/groovy/net/nemerosa/versioning/support/ProcessException.groovy
+++
@@ -1,9 +0,0 @@
-package net.nemerosa.versioning.support;
-
-public abstract class ProcessException extends RuntimeException {
-
-    public ProcessException(String error) {
-        super(error);
-    }
-
-}
--- src/main/groovy/net/nemerosa/versioning/support/ProcessRunException.groovy
+++
@@ -1,9 +0,0 @@
-package net.nemerosa.versioning.support;
-
-public class ProcessRunException extends ProcessException {
-
-    public ProcessRunException(String error) {
-        super(error);
-    }
-
-}
